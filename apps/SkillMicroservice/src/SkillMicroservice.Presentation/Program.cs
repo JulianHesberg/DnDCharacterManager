80daaa4ea3b073e38b8350831c0e1b4463673805
LeeVoi <Abual3a7ed.6@gmail.com>
2025-05-26 10:28:40 +0200

    fixed some errors with all moicroservices

--- apps/SkillMicroservice/src/SkillMicroservice.Presentation/Program.cs
+++ apps/SkillMicroservice/src/SkillMicroservice.Presentation/Program.cs
@@ -1,4 +1,6 @@
 using MessageBroker.Configuration;
+using MessageBroker.Factories;
+using MessageBroker.Interfaces;
 using SkillMicroservice.Infrastructure.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,9 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<MongoDbSettings>(
     builder.Configuration.GetSection("MongoDbSettings"));
 
-builder.Services.Configure<RabbitMQSettings>(
-    builder.Configuration.GetSection("RabbitMQSettings"));
+var rabbitMqOptions = builder.Configuration.GetSection("MessageBrokerOptions").Get<MessageBrokerOptions>();
 
+// Register IMessageBroker using the factory
+builder.Services.AddSingleton<IMessageBroker>(_ => RabbitMQFactory.Create(rabbitMqOptions));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
